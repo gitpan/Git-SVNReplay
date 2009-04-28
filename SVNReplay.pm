89cf8d160bfd4b46457d0c7d5f8235d8a5069c51
Paul Miller <jettero@cpan.org>
2009-04-28 01:51:46 +0000

    Import of JETTERO/Git-SVNReplay-1.0210 from CPAN.
    gitpan-cpan-distribution: Git-SVNReplay
    gitpan-cpan-version:      1.0210
    gitpan-cpan-path:         JETTERO/Git-SVNReplay-1.0210.tar.gz
    gitpan-cpan-author:       JETTERO
    gitpan-cpan-maturity:     released

--- SVNReplay.pm
+++ SVNReplay.pm
@@ -13,7 +13,7 @@ use IPC::System::Simple qw(systemx capturex);
 use Date::Parse; # dates in (Date::Manip will not handle git/date-R time...)
 use POSIX; # dates out
 
-our $VERSION = '1.0200';
+our $VERSION = '1.0210';
 
 our %DEFAULTS = (
     db_file       => "replay.rdb",
@@ -275,7 +275,7 @@ sub add_svn_dir {
 
     unless( -d $r ) {
         ebegin "adding $cod to $this->{svn_co}";
-        eindent
+        eindent;
 
         ebegin "mkdir -p $cod";
         mkpath($r); # uses umask and 0777 to create
@@ -298,7 +298,7 @@ sub add_svn_dir {
         $this->logging_systemx(qw(svn commit -m), "git-svn-replay added $cod to $this->{svn_co}");
         eend 1;
 
-        eoutdent
+        eoutdent;
         eend 1;
     }
 
